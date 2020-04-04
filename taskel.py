5ef8486551e921964a1253f87827744af51a1384
sujay <official.sujaykumar@gmail.com>
2020-04-04 13:47:32 +0530

    added key-based ssh authentication (not tested)

--- taskel.py
+++ taskel.py
@@ -1,45 +1,78 @@
 import os
 
-# Print menu 
+
+# Print menu
 def print_menu():
     print("""
         \t\t\tTaskel: Making tasks simple
         \t\t\t---------------------------
         1. Date
-        2. Calander
+        2. Calender
         3. Create User
         4. Exit
         """)
 
 
-# Method to run commands locally
-def run_local(choice):
-    if choice == '1':
+# Validating entered IP address
+def validate_ip(ip):
+    bytes_sec = ip.split(".")
+    if len(bytes_sec) < 4:
+        return False
+    for byte in bytes_sec:
+        if byte < 0 or byte > 255:
+            return False
+    return True
+
+
+# Method to run commands
+def run(opt):
+    if opt == '1':
         os.system("date")
-    elif choice == '2':
+    elif opt == '2':
         os.system("cal")
-    elif choice == '3':
+    elif opt == '3':
         username = input("Enter the username: ")
         os.system("useradd " + username)
-    elif choice == '4':
+    elif opt == '4':
         exit()
     else:
         print("Option not supported!")
 
 
+# authenticate remote host
+def authenticate(ip):
+    if ip in available_remote:
+        pass
+    print("It seems you have not met before! Please authenticate:\n")
+    os.system("ssh-keygen")
+    os.system("ssh-copy-id root@" + ip)
+    available_remote.append(ip)
+    os.system("ssh " + ip)
+    os.system("clear")
+
+
+# Available connection database
+available_remote = []
+
 if __name__ == "__main__":
     conn = input("""\t\t\tTaskel: Making tasks simple
     \t\t\t---------------------------
     Enter the host IP (use 'localhost' for local terminal): """)
-    while True:
-        # os.system("cls") # toggle comment for Windows
-        os.system("clear") # toggle comment for Linux
-        print_menu()
-        if conn == "localhost":
+
+    while validate_ip(conn) is not True:
+        conn = input("Please enter valid IP in proper format: ")
+    if conn != "localhost":
+        authenticate(conn)
+        while True:
+            os.system("clear")  # toggle comment for Linux
+            print_menu()
+            print("Running on IP: " + conn + " ...")
+            choice = input("Choose an option: ")
+            run(choice)  # remote execution
+            input("Press enter to continue...")
+    else:
+        while True:
             print("Running on localhost...")
             choice = input("Choose an option: ")
-            run_local(choice)
+            run(choice)
             input("Press enter to continue...")
-        else:
-            print("Remote execution is comming soon!")
-        
